271c1eadcec1ecb14437d178341c86fd7547d80b
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-28 20:17:12 +0900

    テストコードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { 'aaaaaa1' }
     password_confirmation { password }
     familiy_name { '小畑' }
     first_name { 'あつし' }
