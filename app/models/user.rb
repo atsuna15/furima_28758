7f819f9027374c523ff85bc20a26d23234c4b36d
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-28 11:50:41 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  validates :nickname, :familiy_name, :first_name, :family_name_k, :first_name_k, :birth, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: ' Include both letters and numbers'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'} do
+    validates :familiy_name
+    validates :first_name
+  end
+
+  with_options presence:true, format: {with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters'} do
+    validates :family_name_k
+    validates :first_name_k
+  end
+
 end
