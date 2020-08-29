1802cbaae208153ab11cffdba7e180c653bfa794
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-29 18:21:26 +0900

    アソシエーション・バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to_active_hash :category, :day, :delivery_price, :region, :status
+  belongs_to :user
+
+  validates :name, :image, :content, :price, :category, :day, :delivery_price, :region, :status, presence: true
+
+  validates :category_id, :day_id, :delivery_price_id, :region_id, :status_id, numericality: { other_than: 1}
 end
