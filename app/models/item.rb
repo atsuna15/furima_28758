b61627dae9ae94afa5995f74c57ccc8f90a9f6bc
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-30 21:57:37 +0900

    ActiveHash ビューの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Association::ActiveRecordExtensions
-  belongs_to_active_hash :category, :day, :delivery_price, :region, :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :delivery_price
+  belongs_to_active_hash :region
+  belongs_to_active_hash :status
   belongs_to :user
 
   validates :name, :image, :content, :price, :category, :day, :delivery_price, :region, :status, presence: true
