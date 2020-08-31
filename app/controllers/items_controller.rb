82e315bc9102324d4b99668f964658c2c8395b6f
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-31 11:14:22 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :content, :price, :category_id, :day_id, :delivery_price_id, :region_id, :status_id)
+  end
 end
