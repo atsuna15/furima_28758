704e065933cd59a65e476395244aac4ad278c813
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-31 14:57:09 +0900

    出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :content, :price, :category_id, :day_id, :delivery_price_id, :region_id, :status_id)
+    params.require(:item).permit(:name, :image, :content, :price, :category_id, :day_id, :delivery_price_id, :region_id, :status_id).merge(user_id: current_user.id)
   end
 end
