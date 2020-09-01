853e217c328aa4eb7f642ec83db63bda796cf597
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-09-01 17:56:44 +0900

    Rubocop

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Item, type: :model do
       @item.image = fixture_file_upload('public/images/sample.jpg')
     end
     describe '商品の出品' do
-      context "商品が出品できる場合" do
+      context '商品が出品できる場合' do
         it 'nameとimage、content、category、status、delivery_price、region、day、priceが存在していれば保存できること' do
           expect(@item).to be_valid
         end
       end
-      context "商品が出品できないとき" do
+      context '商品が出品できないとき' do
         it 'nameが存在しないとき' do
           @item.name = nil
           @item.valid?
@@ -31,27 +31,27 @@ RSpec.describe Item, type: :model do
         it 'category_idが1のとき' do
           @item.category_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include("Category Select")
+          expect(@item.errors.full_messages).to include('Category Select')
         end
         it 'status_idが1のとき' do
           @item.status_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include("Status Select")
+          expect(@item.errors.full_messages).to include('Status Select')
         end
         it 'delivery_price_idが1のとき' do
           @item.delivery_price_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include("Delivery price Select")
+          expect(@item.errors.full_messages).to include('Delivery price Select')
         end
         it 'region_idが1のとき' do
           @item.region_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include("Region Select")
+          expect(@item.errors.full_messages).to include('Region Select')
         end
         it 'day_idが1のとき' do
           @item.day_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include("Day Select")
+          expect(@item.errors.full_messages).to include('Day Select')
         end
         it 'priceが存在しないとき' do
           @item.price = nil
@@ -59,19 +59,19 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Price can't be blank")
         end
         it 'priceが299円以下のとき' do
-          @item.price  = (0..299)
+          @item.price = (0..299)
           @item.valid?
-          expect(@item.errors.full_messages).to include("Price Out of setting range")
+          expect(@item.errors.full_messages).to include('Price Out of setting range')
         end
         it 'priceが1000万円以上のとき' do
-          @item.price = 10000000
+          @item.price = 10_000_000
           @item.valid?
-          expect(@item.errors.full_messages).to include("Price Out of setting range")
+          expect(@item.errors.full_messages).to include('Price Out of setting range')
         end
         it 'priceが全角数字で記入されているとき' do
           @item.price = '３０００'
           @item.valid?
-          expect(@item.errors.full_messages).to include("Price Half-width number")
+          expect(@item.errors.full_messages).to include('Price Half-width number')
         end
       end
     end
