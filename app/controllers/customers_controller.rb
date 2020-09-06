2016a430435016f5672e1567d3b0c7410da279cc
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-09-06 18:20:56 +0900

    購入画面遷移

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,4 +3,10 @@ class CustomersController < ApplicationController
     @item_customer = Item.find(params[:id])
     @item_customer.update(customer_id: current_user.id)
   end
+
+  def index
+    @item = Item.find(params[:id])
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-
   resources :items
+  resources :customers, only: [:index] do
+    member do 
+      get '/', to: 'customers#index'
+    end
+  end
 end
