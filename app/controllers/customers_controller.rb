e00fc7204a49fc1f03208139ee4c8de2a60b9a01
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-09-06 21:44:11 +0900

    出品者は直接購入画面に行けない

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
+  before_action :move_to_index
+
   def done
     @item_customer = Item.find(params[:id])
     @item_customer.update(customer_id: current_user.id)
@@ -8,5 +10,13 @@ class CustomersController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  
+  def move_to_index
+    item = Item.find(params[:id])
+    if current_user == item.user
+      redirect_to root_path
+    end
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
