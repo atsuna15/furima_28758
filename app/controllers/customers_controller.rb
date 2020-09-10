221c04ecb700e61e9eb9ad2437686c0880fda184
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-09-10 22:55:17 +0900

    修正

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -36,8 +36,11 @@ class CustomersController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    if unless user_signed_in?
       redirect_to new_user_session_path
+    end
+    elsif @item.customer || @item.user == current_user
+      redirect_to root_path
     end
   end
 
