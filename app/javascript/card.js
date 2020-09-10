7596875f751ef86278f561285df3630b73fdcc77
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-09-10 21:49:46 +0900

    commit

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,7 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
    };
-
+   
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
