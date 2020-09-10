e291bf8b467fa699f10c1a9d6ff2c9b3e415e277
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-09-10 22:02:13 +0900

    comit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+require("../calc")
+require("../card")
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../calc")
-require("../card")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
